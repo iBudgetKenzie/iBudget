8b4f349b9353821807114e3855bc7d066c45558e
axdborges <axdborges@outlook.com>
2022-08-30 12:05:42 -0300

    feat: estilizando login

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -5,8 +5,17 @@ const Login = () => {
 
     return (
         <StyledLogin>
+            <div className='container'>
+                <h4>Login</h4>
+                <form action="submit">
+                    <label htmlFor="">email:</label>
+                    <input type="text" />
+                    <label htmlFor="">senha:</label>
+                    <input type="text" />
+                    <button type="submit">Entrar</button>
+                </form>
+            </div>
 
-            
         </StyledLogin>
     )
 
--- src/components/Login/styles.ts
+++ src/components/Login/styles.ts
@@ -3,8 +3,12 @@ import styled from "styled-components";
 
 export const StyledLogin = styled.div`
 
-    background-color: gray;
+    background-color: rgba(255, 255, 255, 1);
     height: 100vh;
+
+    .container {
+        background-color: rgba(217, 217, 217, 1);
+    }
 
 `;
 
