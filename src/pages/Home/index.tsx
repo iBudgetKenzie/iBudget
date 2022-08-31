c141b8026fbce209e3b275a7a35f34921dc09aca
axdborges <axdborges@outlook.com>
2022-08-31 15:18:01 -0300

    feat: inicializing HTML structure

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,23 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="body">
+      <section className="logo">
+        IBudget
+        <p className="logo-paragraph">
+          O orçamento que cabe no seu bolso
+        </p>
+      </section>
+      <section className="header">
+        <header>
+          <p className="header-menu">Home</p>
+          <p className="header-menu">Login</p>
+          <p className="header-menu">Cadastro</p>
+          <p className="header-menu">Sobre nós</p>
+        </header>
+        
+      </section>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+export const StyledBody = styled.div`
+
+    width: 100vw;
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column-reverse; 
+
+
+
+`;
