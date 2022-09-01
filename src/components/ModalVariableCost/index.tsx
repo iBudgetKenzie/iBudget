017e2dfe06bbb238f91deaa867b09b4fa095c9ab
gabrielalencar <gabrielalencar33564@gmail.com>
2022-09-01 00:25:25 -0300

    feat: stylization of the fixed price modal and variable price modal

--- src/components/ModalVariableCost/index.tsx
+++ src/components/ModalVariableCost/index.tsx
@@ -3,47 +3,48 @@ import { useForm } from "react-hook-form";
 
 import { BudgetContext } from "../../contexts/BudgetContext";
 import { MoreExpenses } from "../MoreExpenses";
-// import { IMoreExpenses } from "../../contexts/BudgetContext";
+import { IInputs } from "../../contexts/BudgetContext/index";
 
-import { ConteinerModalVariableCost, Modal } from "./style";
+import { ConteinerModalVariableCost, Modal, ConteinerFormModal } from "./style";
 
 export const ModalVariableCost = () => {
-  const { setOnModalVariableCost, addVariableValue, inputs } = useContext(BudgetContext);
+  const { setOnModalVariableCost, addVariableValue, inputs } =
+    useContext(BudgetContext);
 
-  const {register, handleSubmit} = useForm()
+  const { register, handleSubmit } = useForm();
 
   return (
     <ConteinerModalVariableCost>
       <Modal>
         <div>
-          <h2>Custo variável</h2>
+          <h2>Custos variáveis</h2>
           <span onClick={() => setOnModalVariableCost(false)}>X</span>
         </div>
-        <div>
-        <form onSubmit={handleSubmit(addVariableValue)}>
-            {inputs.map((index) => {
+        <ConteinerFormModal>
+          <form onSubmit={handleSubmit(addVariableValue)}>
+            {inputs.map(({ example, name, title }: IInputs, index) => {
               return (
                 <MoreExpenses key={index}>
-                  <label htmlFor="value">Valor:</label>
+                  <label htmlFor="value">{title}</label>
                   <input
                     type="number"
                     id="value"
-                    placeholder="Ex: 1,00R$"
-                    {...register(index)}
+                    placeholder={example}
+                    {...register(name)}
                   />
 
-                  <label htmlFor="description">Descrição custo:</label>
+                  {/* <label htmlFor="description">Descrição custo:</label>
                   <input
                     type="text"
                     placeholder="Ex: Aluguel"
                     id="description"
-                  />
+                  /> */}
                 </MoreExpenses>
               );
             })}
             <button type="submit">Calcular custos</button>
           </form>
-        </div>
+        </ConteinerFormModal>
       </Modal>
     </ConteinerModalVariableCost>
   );
---
+++ src/components/MoreExpenses/style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const InputMoreExpenses = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    & > label {
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 15px;
+    }
+
+    & > input {
+        height: 30px;
+        border: 1px solid rgba(var(--color-purple), 1);
+        background-color: rgba(var(--color-purple-light), 0.10);
+        border-radius: 5px;
+        
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 300;
+        font-size: 14px;
+        line-height: 12px;
+        padding: 17px 0px 17px 10px;
+    }
+`
