eb9bf35fd78ab66049c1cfa140d87178f8a024ad
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2022-09-02 18:31:28 -0300

    feat: adding equipe pictures and the AboutUs component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,13 @@
 import { useUserContext } from "../../contexts/UserContext";
 import HomeComponent from "../../components/Home";
 import LoginModal from "../../components/Login";
+import AboutUs from "../../components/AboutUs";
 
 import { StyledBody } from "./styles";
 import logo from "../../assets/img/logo.svg";
 import Register from "../../components/Register";
 
-
-
 const Home = () => {
-
   const {
     isHome,
     isLogin,
@@ -22,51 +20,58 @@ const Home = () => {
   } = useUserContext();
 
   const handleHome = () => {
-    setIsHome(true)
-    setIsLogin(false)
-    setIsRegister(false)
-    setIsSobre(false)
+    setIsHome(true);
+    setIsLogin(false);
+    setIsRegister(false);
+    setIsSobre(false);
   };
 
   const handleLogin = () => {
-    setIsHome(false)
-    setIsLogin(true)
-    setIsRegister(false)
-    setIsSobre(false)
+    setIsHome(false);
+    setIsLogin(true);
+    setIsRegister(false);
+    setIsSobre(false);
   };
   const handleCadastro = () => {
-    setIsHome(false)
-    setIsLogin(false)
-    setIsRegister(true)
-    setIsSobre(false)
+    setIsHome(false);
+    setIsLogin(false);
+    setIsRegister(true);
+    setIsSobre(false);
   };
   const handleSobre = () => {
-    setIsHome(false)
-    setIsLogin(false)
-    setIsRegister(false)
-    setIsSobre(true)
+    setIsHome(false);
+    setIsLogin(false);
+    setIsRegister(false);
+    setIsSobre(true);
   };
-  
+
   return (
     <StyledBody>
       <section className="logo-div">
         <img src={logo} alt="" />
-        <p className="logo-paragraph">
-          O orçamento que cabe no seu bolso
-        </p>
+        <p className="logo-paragraph">O orçamento que cabe no seu bolso</p>
       </section>
       <section className="header">
         <header>
-          <button className="header-menu" onClick={handleHome}>Home</button>
-          <button className="header-menu" onClick={handleLogin}>Login</button>
-          <button className="header-menu" onClick={handleCadastro}>Cadastro</button>
-          <button className="header-menu" onClick={handleSobre}>Sobre nós</button>
+          <button className="header-menu" onClick={handleHome}>
+            Home
+          </button>
+          <button className="header-menu" onClick={handleLogin}>
+            Login
+          </button>
+          <button className="header-menu" onClick={handleCadastro}>
+            Cadastro
+          </button>
+          <button className="header-menu" onClick={handleSobre}>
+            Sobre nós
+          </button>
         </header>
 
         <div className="content">
-          { isHome && <HomeComponent/> }
-          { isLogin && <LoginModal/> }
-          { isRegister && <Register/>}
+          {isHome && <HomeComponent />}
+          {isLogin && <LoginModal />}
+          {isRegister && <Register />}
+          {isSobre && <AboutUs />}
         </div>
       </section>
     </StyledBody>
