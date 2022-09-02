1fc1ec63103b46e681517562a9e0468bf1d31c19
Álvaro Romário Cavalcante Alencar <alvaroromario1@hotmail.com>
2022-09-02 10:41:44 -0300

    fix: fixing tab width of 2 spaces

--- src/components/CardBudgetHistory/style.ts
+++ src/components/CardBudgetHistory/style.ts
@@ -1,53 +1,52 @@
 import styled from "styled-components";
 
 export const ConteinerCardBudgetHistory = styled.li`
-    height: 89px;
+  height: 89px;
 
-    border: 1px solid rgba(var(--color-purple), 1);
-    background-color: rgba(var(--color-purple), 0.45);
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-    border-radius: 8px;
-    padding: 16px 16px 16px 20px;
-    
-    position: relative;
+  border: 1px solid rgba(var(--color-purple), 1);
+  background-color: rgba(var(--color-purple), 0.45);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+  padding: 16px 16px 16px 20px;
 
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 4px;
+
+  span {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+    color: #474545;
+  }
+
+  & > h2 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 17px;
+  }
+
+  & > span + span {
+    font-weight: 200;
+    color: #524e4e;
+  }
+
+  & > div {
+    position: absolute;
+    right: 15px;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
-    gap: 4px;
+    gap: 10px;
 
-    span {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 12px;
-        line-height: 15px;
-        color: #474545;
+    & > svg {
+      width: 20px;
+      height: 20px;
     }
-
-    & > h2 {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 14px;
-        line-height: 17px;
-    }
-
-    & > span + span {
-        font-weight: 200;
-        color: #524E4E;
-    }
-
-    & > div {
-        position: absolute;
-        right: 15px;
-        display: flex;
-        gap: 10px;
-
-        & > svg {
-            width: 20px;
-            height: 20px;
-
-        }
-    }
-`
+  }
+`;
--- src/components/CreateBudget/styles.ts
+++ src/components/CreateBudget/styles.ts
@@ -1,197 +1,198 @@
 import styled from "styled-components";
 
 export const ContainerCreateBudget = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 368px;
+  max-height: 540px;
+  padding: 21px 30px 30px 30px;
+  background-color: rgba(var(--color-white), 1);
+
+  border: 1px solid rgba(var(--color-purple), 1);
+  border-radius: 8px;
+
+  & h1 {
+    font-family: "Inter";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 24px;
+
+    padding-bottom: 20px;
+  }
+
+  & > form {
     display: flex;
     flex-direction: column;
-    align-items: center;
 
-    width: 368px;
-    max-height: 540px;
-    padding: 21px 30px 30px 30px;
+    width: 100%;
+    height: 100%;
+    gap: 5px;
     background-color: rgba(var(--color-white), 1);
 
-    border: 1px solid rgba(var(--color-purple), 1);
     border-radius: 8px;
 
-    & h1 {
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 600;
-        font-size: 20px;
-        line-height: 24px;
-
-        padding-bottom: 20px;
+    textarea:focus,
+    input:focus {
+      box-shadow: 0 0 0 0;
+      outline: 0;
     }
 
-    & > form {
-        display: flex;
-        flex-direction: column;
+    label {
+      font-family: "Inter";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 12px;
+      line-height: 15px;
 
+      display: flex;
+      align-items: flex-end;
+    }
+
+    span {
+      color: rgba(var(--color-negative-pink), 1);
+      padding-left: 2px;
+    }
+
+    input {
+      height: 39px;
+      border: 1px solid rgba(var(--color-purple), 1);
+      border-radius: 5px;
+      padding-left: 6px;
+
+      font-family: "Inter";
+      font-style: normal;
+      font-weight: 300;
+      font-size: 13px;
+      line-height: 16px;
+      display: flex;
+      align-items: flex-end;
+      color: #786d6d;
+    }
+
+    & > div {
+      height: 39px;
+      max-width: 100%;
+      display: flex;
+      align-items: center;
+
+      font-family: "Inter";
+      font-size: 15px;
+
+      position: relative;
+
+      & > input {
         width: 100%;
         height: 100%;
-        gap: 5px;
-        background-color: rgba(var(--color-white), 1);
+        position: absolute;
 
-        border-radius: 8px;
+        border: 1px solid rgba(var(--color-purple), 1);
+        border-radius: 5px;
+        padding-left: 6px;
+      }
 
-        textarea:focus, input:focus {
-            box-shadow: 0 0 0 0;
-            outline: 0;
-        }
+      & > svg {
+        position: absolute;
+        right: 12px;
+        top: 10px;
+        width: 16px;
+        height: 16px;
 
-        label {
-            font-family: 'Inter';
-            font-style: normal;
-            font-weight: 400;
-            font-size: 12px;
-            line-height: 15px;
-
-            display: flex;
-            align-items: flex-end;
-        }
-        
-        span {
-            color: rgba(var(--color-negative-pink), 1);
-            padding-left: 2px;
-        }
-
-        input {
-            height: 39px;
-            border: 1px solid rgba(var(--color-purple), 1);
-            border-radius: 5px;
-            padding-left: 6px;
-
-            font-family: 'Inter';
-            font-style: normal;
-            font-weight: 300;
-            font-size: 13px;
-            line-height: 16px;
-            display: flex;
-            align-items: flex-end;
-            color: #786D6D;
-        }
-
-        & > div {
-            height: 39px;
-            max-width: 100%;
-            display: flex;
-            align-items: center;
-            
-            font-family: "Inter";
-            font-size: 15px;
-
-            position: relative;
-
-            & > input {
-                width: 100%;
-                height: 100%;
-                position: absolute;
-
-                border: 1px solid rgba(var(--color-purple), 1);
-                border-radius: 5px;
-                padding-left: 6px;
-            }
-
-            & > svg {
-                position: absolute;
-                right: 12px;
-                top: 10px;
-                width: 16px;
-                height: 16px;
-
-                cursor: pointer;
-            }
-        }
-
-        & button {
-            background-color: rgba(var(--color-purple), 1);
-            border-radius: 8px;
-            height: 48px;
-            margin-top: 22px;
-            border: none;
-
-            font-family: 'Inter';
-            font-style: normal;
-            font-weight: 400;
-            font-size: 14px;
-            line-height: 17px;
-
-            color: aliceblue;
-
-            cursor: pointer;
-
-            &:hover {
-                background-color: rgba(var(--color-purple-light), 1);
-            }
-      } 
-    } 
-
-    @media (max-width: 375px) {
-        width: 100vw;
+        cursor: pointer;
+      }
     }
-`
+
+    & button {
+      background-color: rgba(var(--color-purple), 1);
+      border-radius: 8px;
+      height: 48px;
+      margin-top: 22px;
+      border: none;
+
+      font-family: "Inter";
+      font-style: normal;
+      font-weight: 400;
+      font-size: 14px;
+      line-height: 17px;
+
+      color: aliceblue;
+
+      cursor: pointer;
+
+      &:hover {
+        background-color: rgba(var(--color-purple-light), 1);
+      }
+    }
+  }
+
+  @media (max-width: 375px) {
+    width: 100vw;
+  }
+`;
 
 export const ConteinerDate = styled.div`
-    margin-top: 8px;
-    display: flex;
-    justify-content: space-between;
+  margin-top: 8px;
+  display: flex;
+  justify-content: space-between;
 
-    & > div {
-        height: 55px;
-        width: 140px;
+  & > div {
+    height: 55px;
+    width: 140px;
 
-        & label {
-            padding-bottom: 5px;
-        }
-
-        & > input {
-            display: flex;
-            align-items: center;
-
-            width: 100%;
-            padding: 0 6px;
-        }
+    & label {
+      padding-bottom: 5px;
     }
 
-    @media (max-width: 375px) {
-        & div {
-            width: 38vw;
-        }
+    & > input {
+      display: flex;
+      align-items: center;
+
+      width: 100%;
+      padding: 0 6px;
     }
-`
+  }
+
+  @media (max-width: 375px) {
+    & div {
+      width: 38vw;
+    }
+  }
+`;
 
 export const ConteinerWorkTime = styled.div`
-    margin-top: 23px;
-    display: flex;
-    justify-content: space-between;
+  margin-top: 23px;
+  display: flex;
+  justify-content: space-between;
 
-    & > div {
-        height: 55px;
-        width: 140px;
+  & > div {
+    height: 55px;
+    width: 140px;
 
-        & label {
-            padding-bottom: 5px;
-        }
-
-        & > input {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-
-            width: 100%;
-            padding: 0 6px;
-        }
+    & label {
+      padding-bottom: 5px;
     }
 
-    & > div + div {
-        & > input {
-            padding-left: 2.9rem;
-        }
-    }
+    & > input {
+      display: flex;
+      align-items: center;
+      justify-content: center;
 
-    @media (max-width: 375px) {
-        & div {
-            width: 38vw;
-        }
+      width: 100%;
+      padding: 0 6px;
     }
-`
+  }
+
+  & > div + div {
+    & > input {
+      padding-left: 2.9rem;
+    }
+  }
+
+  @media (max-width: 375px) {
+    & div {
+      width: 38vw;
+    }
+  }
+`;
