b90c9aa13ea887a61b1d3a43c969da401bdeb590
gabrielalencar <gabrielalencar33564@gmail.com>
2022-09-03 02:22:28 -0300

    fix: quote listing with deletion request and pdf

--- src/components/BudgetHistory/index.tsx
+++ src/components/BudgetHistory/index.tsx
@@ -1,13 +1,13 @@
 import { CardBudgetHistory } from "../CardBudgetHistory";
-import { useUserContext } from "../../contexts/UserContext/index";
 import { IBudget } from "../../contexts/UserContext/index";
+import { useBudgetContext } from "../../contexts/BudgetContext";
 
 import { IoSearch } from "react-icons/io5";
 
 import { ContainerBudgetHistory, FilterBar } from "./style";
 
 export const BudgetHistory = () => {
-  const { budgetHistory } = useUserContext()
+  const { budgetHistory } = useBudgetContext();
 
   return (
     <ContainerBudgetHistory>
@@ -22,19 +22,24 @@ export const BudgetHistory = () => {
       </FilterBar>
 
       <ul>
-        {
-          budgetHistory && budgetHistory.length === 0 ? 
-          (
-            <p>Ops, não existe orçamento ainda aqui!</p>
-          ): (
-            budgetHistory?.map(({budget, projectName, id}: IBudget) => {
-              return <CardBudgetHistory key={id} projectName={projectName} budget={budget} id={id}/>
-            })
-          ) 
-        }
+        {budgetHistory && budgetHistory.length === 0 ? (
+          <p>Ops, não existe orçamento ainda aqui!</p>
+        ) : (
+          budgetHistory?.map(
+            ({ budget, projectName, id, projectTime }: IBudget) => {
+              return (
+                <CardBudgetHistory
+                  key={id}
+                  projectName={projectName}
+                  budget={budget}
+                  id={id}
+                  projectTime={projectTime}
+                />
+              );
+            },
+          )
+        )}
       </ul>
     </ContainerBudgetHistory>
   );
 };
-
-// , fixedCost, projectTime, userId, variableCost
