e74a734e6750597f1020a8d14f70cabac8612a4b
Victor <ovictoravila@gmail.com>
2022-09-04 12:51:38 -0300

    feat: fix About Us component and css

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -12,149 +12,162 @@ const AboutUs = () => {
     <>
       <AboutUsDiv>
         <p>
-          Este é o Projeto final Front-End do M3 da Kenzie Academy Brasil (turma
-          13). Queriamos agradecer a toda nossa turma e a equipe da Kenzie, em
-          especial ao facilitador Daniel Pereira e o nosso instrutor Gabriel
-          Tsunoda. Segue abaixo o time do grupo 3 que foram os responsáveis pela
-          idealização e desenvolvimento deste aplicativo.
+          Este é o nosso Projeto final Front-End do M3 da Kenzie Academy Brasil
+          (turma 13). Nós do grupo 3 gostaríamos de agradecer a toda nossa turma
+          e a equipe da Kenzie, em especial ao facilitador Daniel Pereira e ao
+          nosso instrutor Gabriel Tsunoda. Segue abaixo o time responsável pela
+          idealização e desenvolvimento deste aplicativo. Aos que nos
+          prestigiaram fica o nosso muito obrigado!
         </p>
         <EquipList>
           <Dev>
             <figure>
               <img src={victor} alt="Victor Avila - Scrum Master" />
             </figure>
-            <p>Victor Ávila</p>
-            <p>Scrum Master</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/victor-avila-br/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/victoraviladev"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Victor Ávila</p>
+              <p className="dev-job">Scrum Master</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/victor-avila-br/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/victoraviladev"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
           <Dev>
             <figure>
               <img src={alexandre} alt="Alexandre Borges - Product Owner" />
             </figure>
-            <p>Alexandre Borges</p>
-            <p>Product Owner</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/alexandre-borges-albuquerque/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/axdborges"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Alexandre Borges</p>
+              <p className="dev-job">Product Owner</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/alexandre-borges-albuquerque/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/axdborges"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
           <Dev>
             <figure>
               <img src={alvaro} alt="Alvaro Alencar - Tech Leader" />
             </figure>
-            <p>Álvaro Alencar</p>
-            <p>Tech Leader</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/alvaro-romario-cavalcante-alencar/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/alvaroallencar"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Álvaro Alencar</p>
+              <p className="dev-job">Tech Leader</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/alvaro-romario-cavalcante-alencar/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/alvaroallencar"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
           <Dev>
             <figure>
               <img src={gabriel} alt="Gabriel Alencar - Quality Assurance" />
             </figure>
-            <p>Gabriel Alencar</p>
-            <p>Quality Assurance</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/gabriel-alencar-de-souza-santos/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/gabrielAlencar33564"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Gabriel Alencar</p>
+              <p className="dev-job">Quality Assurance</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/gabriel-alencar-de-souza-santos/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/gabrielAlencar33564"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
           <Dev>
             <figure>
               <img src={gabriella} alt="Gabriella Terra - Quality Assurance" />
             </figure>
-            <p>Gabriella Terra</p>
-            <p>Quality Assurance</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/gabriella-terra/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/GabriellaTerra"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Gabriella Terra</p>
+              <p className="dev-job">Quality Assurance</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/gabriella-terra/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/GabriellaTerra"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
           <Dev>
             <figure>
               <img src={rafael} alt="Rafael Grillo - Quality Assurance" />
             </figure>
-            <p>Rafael Grillo</p>
-            <p>Quality Assurance</p>
             <div>
-              <a
-                href="https://www.linkedin.com/in/rafael-grillo/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoLinkedin />
-              </a>
-              <a
-                href="https://github.com/rGrillo1"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <IoLogoGithub />
-              </a>
+              <p className="dev-name">Rafael Grillo</p>
+              <p className="dev-job">Quality Assurance</p>
+              <div>
+                <a
+                  href="https://www.linkedin.com/in/rafael-grillo/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoLinkedin />
+                </a>
+                <a
+                  href="https://github.com/rGrillo1"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoLogoGithub />
+                </a>
+              </div>
             </div>
           </Dev>
         </EquipList>
