a2bd2a04b25bb66e448030940c15e13c005920ef
Álvaro Alencar <alvaroromario1@hotmail.com>
2022-09-10 02:14:00 -0300

    style: change intensity of projectNumber text

--- src/components/CardBudgetHistory/style.ts
+++ src/components/CardBudgetHistory/style.ts
@@ -5,7 +5,7 @@ export const ConteinerCardBudgetHistory = styled.li`
 
   border: 1px solid rgba(var(--color-purple), 1);
   background-color: rgba(var(--color-purple), 0.45);
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 4px 4px rgba(var(--color-black), 0.25);
   border-radius: 8px;
   padding: 16px 16px 16px 20px;
 
@@ -24,7 +24,7 @@ export const ConteinerCardBudgetHistory = styled.li`
     font-weight: 400;
     font-size: 12px;
     line-height: 15px;
-    color: rgba(var(--color-grey-2), 1);
+    color: rgba(var(--color-grey-3), 1);
 
     white-space: nowrap;
     overflow: hidden;
@@ -45,7 +45,7 @@ export const ConteinerCardBudgetHistory = styled.li`
 
   & > span + span {
     font-weight: 200;
-    color: rgba(var(--color-grey-1), 1);
+    color: rgba(var(--color-grey-2), 1);
   }
 
   & > div {
